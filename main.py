7bd75690c8655c53eb4509642cf1d25e4270d579
Bomi45 <eldacollege40@yahoo.com>
2023-08-26 00:14:26 -0700

    commit

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    original_str = "Python Programming"
+    sub1 = original_str[: -12]
+    sub2 = (original_str.split()[1])
+    merged_str = sub2 + sub1
+    
+    print(sub2)
+    print(sub1)
+    print(merged_str)
+    
     pass
-
+ 
 
 if __name__ == '__main__':
     main()
